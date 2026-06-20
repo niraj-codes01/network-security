bfcf874773bfa67565bb7b95cc9216f181b4ec3d
niraj-codes01 <nirajpachpande1681@gmail.com>
2026-06-20 19:37:31 +0530

    Data Validation

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -31,3 +31,23 @@ class DataIngestionConfig:
         self.train_test_split_ratio:float=training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name:str= training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str=training_pipeline.DATA_INGESTION_DATABASSE_NAME
+
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipeline_Config):
+        self.data_validation_dir:str=os.path.join(
+            training_pipeline_config.artifact_dir,training_pipeline.DATA_VALIDATION_DIR_NAME )
+        self.invalid_data_dir:str=os.path.join(
+            self.data_validation_dir,training_pipeline.DATA_VALIDATION_DIR_NAME,training_pipeline.FILE_NAME )
+        self.valid_data_dir:str=os.path.join(
+            self.data_validation_dir,training_pipeline.DATA_VALIDATION_DIR_NAME,training_pipeline.FILE_NAME)
+        self.valid_training_file_path:str=os.path.join(
+            self.valid_data_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.valid_testing_file_path:str=os.path.join(
+            self.valid_data_dir,training_pipeline.TEST_FILE_NAME)
+        self.invalid_training_file_path:str=os.path.join(
+            self.invalid_data_dir,training_pipeline.TRAIN_FILE_NAME )
+        self.invalid_testing_file_path:str=os.path.join(
+            self.invalid_data_dir,training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path:str=os.path.join(
+            self.data_validation_dir,training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+        

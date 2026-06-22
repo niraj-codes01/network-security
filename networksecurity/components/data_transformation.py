f8cc34a58331ac508abe77b30d5075728a60be6b
niraj-codes01 <nirajpachpande1681@gmail.com>
2026-06-22 19:43:38 +0530

    Data Transformation

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from networksecurity.entity.artifact_entity import (
+    DataIngestionArtifact,
+    DataValidationArtifact
+)
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_numpy_array_data,save_object
+
+class DataTransformation:
+    def __init__(self,):
+        pass
+
